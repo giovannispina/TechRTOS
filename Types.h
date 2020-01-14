630cef90da96cea3cb6e53e2b8645a29d070ac88
GIOVANNI-PC\Giovanni <giovannispina93@live.com>
2020-01-14 01:44:45 +0100

    Remove old license header
    Remove old license header

--- Types.h
+++ Types.h
@@ -1,47 +1,22 @@
-/* Copyright (c) 2014, Giovanni Spina <giovanni@codeforfun.it>
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of the uploader nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY GIOVANNI SPINA ``AS IS'' AND ANY
-* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL GIOVANNI SPINA BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
-
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
 #ifndef NULL
-	#define NULL 0
+#define NULL 0
 #endif
 
 #define true 1
 #define false 0
 
-#define uint8	unsigned char
-#define int8	signed short
+#define uint8 unsigned char
+#define int8 signed short
 
-#define uint16	unsigned int
-#define int16	signed int
+#define uint16 unsigned int
+#define int16 signed int
 
-#define uint32	unsigned long
-#define int32	signed long
+#define uint32 unsigned long
+#define int32 signed long
 
-#define bool	uint8
+#define bool uint8
 
 #endif /* __TYPES_H__ */
